600f7e1874f4b1548771f7b74f649f45b9202fde
Ar_Monarch <praffulthapa12@gmail.com>
2025-04-03 00:19:23 +0545

    help functions that generate documentations started

---
+++ crates/cli/src/help.rs
@@ -0,0 +1,9 @@
+/// Generate a short documentation.
+pub fn help_short() -> String {
+    unimplemented!("Not Implemented")
+}
+
+/// Generate long Documentation.
+pub fn help_long() -> String {
+    unimplemented!("Not Implemented")
+}
