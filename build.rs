eb1a3e5d19c203ffe0bdae97443896463930bc26
Ar_Monarch <praffulthapa12@gmail.com>
2025-04-02 22:50:19 +0545

    feat: function to get current git hash and set it to cargo env variable to be used for HelpLong

--- build.rs
+++ build.rs
@@ -1,1 +1,21 @@
-fn main() {}
+fn main() {
+    set_git_hash();
+}
+
+/// Make the current git hash available to the build as the environment variable
+/// `POEKMON_TERM_BUILD_GIT_HASH`.
+fn set_git_hash() {
+    use std::process::Command;
+
+    let args = &["rev-parse", "HEAD"];
+    let Ok(output) = Command::new("git").args(args).output() else {
+        return;
+    };
+
+    let str = String::from_utf8_lossy(&output.stdout).to_string();
+    if str.is_empty() {
+        return;
+    }
+
+    println!("cargo:rustc-env=POEKMON_TERM_BUILD_GIT_HASH={}", str);
+}
