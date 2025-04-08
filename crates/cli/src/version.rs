49abbe30235ef67bb0c9f918f9efe451610c131a
Ar_Monarch <praffulthapa12@gmail.com>
2025-04-08 20:04:38 +0545

    gradually push version generation data strings insted of directly returning the formatted string

--- crates/cli/src/version.rs
+++ crates/cli/src/version.rs
@@ -1,3 +1,7 @@
+macro_rules! write {
+    ($($tt:tt)*) => { std::write!($($tt)*).unwrap();}
+}
+
 /// Generate a short Version strign in the form `pokemon-term x.y.z`
 pub fn version_short() -> String {
     let app_name = "pokemon-term";
@@ -10,14 +14,18 @@ pub fn version_short() -> String {
 /// This includes not only the version of pokemon-term but some other information about its build.
 /// For example, Git rev hash
 pub fn version_long() -> String {
+    use std::fmt::Write;
+
+    let mut output = String::new();
+
     let app_name = "pokemon-term";
     let version = option_env!("CARGO_PKG_VERSION").unwrap_or("N/A");
     let authors = option_env!("CARGO_PKG_AUTHORS").unwrap_or("N/A");
     let git_hash = option_env!("POKEMON_TERM_BUILD_GIT_HASH").unwrap_or("");
-    format!(
-        r"
-{app_name} {version} (rev {git_hash})
-{authors}
-"
-    )
+
+    write!(output, "{app_name} {version} (rev {git_hash})");
+    write!(output, "\n");
+    write!(output, "{authors}");
+
+    output
 }
